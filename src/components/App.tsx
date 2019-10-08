c25d4e3a3f3983a71b57bbb5852109bdfeb19fd7
Afief Sholichuddin <apiep.oedin@gmail.com>
2019-10-08 11:33:15 +0700

    chore: add commit lint and prettier

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react"
+
+export function App() {
+	return (
+		<div className="App">
+			<ul className="list">
+				<li>Satu</li>
+				<li>Dua</li>
+			</ul>
+		</div>
+	)
+}
